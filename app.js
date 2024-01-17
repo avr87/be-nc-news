5ef0e8d755c9bb8b23a3d07a12d30707685ea3bf
avr87 <annavanruiten@gmail.com>
2024-01-17 12:53:47 +0000

    show completed task 07-post-api-articles-article_id-comments

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const {
   getArticleById,
   getArticles,
   getCommentsByArticleId,
+  postCommentByArticleId,
 } = require("./controllers/controllers");
 
-
+app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
@@ -21,6 +22,8 @@ app.get("/api/articles", getArticles);
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
+
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
@@ -28,7 +31,13 @@ app.use((err, req, res, next) => {
     next(err);
   }
 });
-
+app.use((err, req, res, next) => {
+  if (err.code === "23503") {
+    res.status(404).send({ msg: "No article available" });
+  } else {
+    next(err);
+  }
+});
 app.use((err, req, res, next) => {
   if (err.code === "42703" || err.code === "22P02") {
     res.status(400).send({ msg: "Bad request" });
