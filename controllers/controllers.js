0fbed2cc0644b84bd4c81717f6196ec4ba04939f
avr87 <annavanruiten@gmail.com>
2024-04-02 11:47:19 +0100

    show added req.queries to getArticles()

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -36,8 +36,8 @@ exports.getArticleById = (req, res, next) => {
 };
 
 exports.getArticles = (req, res, next) => {
-  const { topic } = req.query;
-  fetchArticles(topic)
+  const { topic, sort_by, order } = req.query;
+  fetchArticles(topic, sort_by, order)
     .then((articles) => {
       res.status(200).send({ articles });
     })
